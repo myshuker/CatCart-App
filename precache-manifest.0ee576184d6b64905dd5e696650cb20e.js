81e1f9e487a3b8991742fc0a3210211a861e69cf
myshuker <myshuker@gmai.com>
2020-08-17 02:05:38 +0200

    Updates

---
+++ precache-manifest.0ee576184d6b64905dd5e696650cb20e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c42d394c1a9b2c0a927dffcf58fbb9b",
+    "url": "/CatCart-App/index.html"
+  },
+  {
+    "revision": "8f0de37546752a713066",
+    "url": "/CatCart-App/static/css/main.89b790b6.chunk.css"
+  },
+  {
+    "revision": "bbf7505f05cf6026f076",
+    "url": "/CatCart-App/static/js/2.2921720c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CatCart-App/static/js/2.2921720c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0de37546752a713066",
+    "url": "/CatCart-App/static/js/main.75fef76a.chunk.js"
+  },
+  {
+    "revision": "f7b228fa451175d96b0c",
+    "url": "/CatCart-App/static/js/runtime-main.df7e198c.js"
+  }
+]);
